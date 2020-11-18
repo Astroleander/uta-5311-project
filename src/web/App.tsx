2ab05feea31083712fe9a57b61be865d4aa0c9d5
Astroleander <astroleander@gmail.com>
2020-11-18 23:39:29 +0800

    build base frame

--- src/web/App.tsx
+++ src/web/App.tsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Input from './Input';
+import Output from './Output';
 
 export default function App() {
+  const [input, setInput] = useState(() => new Array(10).map(() => ~~Math.random()));
+  const onInputChanged = (newInput) => {
+    setInput(newInput);
+  }
+
   return (
     <article>
       {/* <ViewRouter /> */}Final Project
+      <Input input={input} onInputChanged={onInputChanged} />
+      <Output input={input} />
     </article>
   )
 }
---
+++ src/web/Output.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Output({ input }) {
+  return <>Output</>
+}
